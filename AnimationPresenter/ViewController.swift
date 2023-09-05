c071ae6d0676f28a6cf64e428eeb12985885671b
K-ima <bulgin.a.v@bk.ru>
2023-09-05 10:56:24 +0300

    Initial Commit

---
+++ AnimationPresenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationPresenter
+//
+//  Created by Kima on 05.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
